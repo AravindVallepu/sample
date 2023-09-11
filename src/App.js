e2612030d0e9252353bc49dcd9a399597fd65058
AravindVallepu <111575603+AravindVallepu@users.noreply.github.com>
2023-09-11 08:02:42 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,14 +4,30 @@ import Home from "./Pages/Home";
 //import About from "./Pages/About";
 //import Dashboard from "./Pages/Dashboard";
 
-export default function App() {
+// export default function App() {
+//   return (
+//     <div>
+//       <BrowserRouter>
+//         <Routes>
+//           <Route index element={<Home />} />
+//         </Routes>
+//       </BrowserRouter>
+//     </div>
+//   );
+// }
+
+const App = () => {
   return (
     <div>
       <BrowserRouter>
+        <Navbar />
         <Routes>
-          <Route index element={<Home />} />
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
         </Routes>
       </BrowserRouter>
     </div>
   );
-}
+};
+
+export default App;
